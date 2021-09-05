e825c19a15b2271300bb4f39af45906ce9c8b8fc
divyanshipatel123 <79079219+divyanshipatel123@users.noreply.github.com>
2021-09-05 09:37:19 +0530

    Update Game.js

--- jsFile/Game.js
+++ jsFile/Game.js
@@ -11,9 +11,9 @@ class Game{
     }
 
  start(){
-     textSize(30);
+     textSize(35);
      fill('green');
-     text('THE RECYCLER GAME' , displayWidth/2 - 75 , displayHeight/2 - 300 );
+     text('THE RECYCLER GAME' , displayWidth/2 - 100 , displayHeight/2 - 350 );
    //background(this.homescreenImg);
    textSize(25);
    fill('black');
